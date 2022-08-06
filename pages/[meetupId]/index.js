1b62f487520de626e98423e80db471cbd77262df
Shahaj <gshahaj@gmail.com>
2022-08-06 22:20:51 +0600

    Meetup details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/da/Kyiv_Symphony_Orchestra%2C_Kurhaus_Wiesbaden.jpg"
+      title="A First Meetup"
+      address="Some Address 5, 12345 some city"
+      description="This is first meetup"
+    />
+  );
+}
